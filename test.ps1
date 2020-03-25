98cdb6c53c2c940d1055e809ed5aedc4f25035e8
Howard Chiam <hchiam@users.noreply.github.com>
2020-03-25 18:11:01 -0400

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,11 @@
+# you can write a message to the console:
+Write-Host 'Hi!'
+
+# you can open a website in the user's default browser:
+start https://microsoft.com
+
+# you can download a file:
+Invoke-WebRequest https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -OutFile ./install-python.exe
+
+# you can run an exe:
+./install-python.exe

eadaae1ef233e51389a397ca25f015b04e716dfb
Howard Chiam <hchiam@users.noreply.github.com>
2020-03-25 18:44:42 -0400

    add alternative to open url

--- test.ps1
+++ test.ps1
@@ -2,7 +2,7 @@
 Write-Host 'Hi!'
 
 # you can open a website in the user's default browser:
-start https://microsoft.com
+start https://microsoft.com # or: open https://microsoft.com
 
 # you can download a file:
 Invoke-WebRequest https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -OutFile ./install-python.exe

d26fed8306445c4479a37e78e45ee4c2f3de4479
hchiam <hchiam@gmail.com>
2020-03-28 09:06:41 -0400

    add more examples

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ Just one of the things I'm learning. <https://github.com/hchiam/learning>
 
 Related repo: <https://github.com/hchiam/learning-bash-scripts>
 
+Once you're in PowerShell, you can run the `test.ps1` file:
+
+```powershell
+cd learning-powershell
+./test.ps1
+```
+
 ## For MacOS
 
 ```bash
--- test.ps1
+++ test.ps1
@@ -2,10 +2,46 @@
 Write-Host 'Hi!'
 
 # you can open a website in the user's default browser:
-start https://microsoft.com # or: open https://microsoft.com
+# start https://microsoft.com # or: open https://microsoft.com
+
+# you can navigate to folders like the desktop:
+cd ~/Desktop
 
 # you can download a file:
-Invoke-WebRequest https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -OutFile ./install-python.exe
+# Invoke-WebRequest https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -OutFile ./install-python.exe
 
 # you can run an exe:
-./install-python.exe
+# ./install-python.exe
+
+# you can create and run a functino:
+function wait3seconds {
+  Start-Sleep -s 3
+}
+
+Write-Host 'This message should show up before the 3-second delay.'
+wait3seconds
+Write-Host 'This message should show up after 3 seconds.'
+
+# here's a more complex example:
+function installPythonIfMissing {
+  $p = & { python -V } 2>&1
+  if ($p -is [System.Management.Automation.ErrorRecord]) {
+    Write-Host 'Python is not set up or installed. Attempting install now.'
+    downloadPythonInstaller
+    openPythonGraphicalInstaller
+  }
+  elseif ($PythonVersion -ne 3.7.7) {
+    Write-Host 'Python 3.7.7 is not set up or installed. Attempting install now.'
+    downloadPythonInstaller
+    openPythonGraphicalInstaller
+  }
+}
+
+function installDependencies {
+  # pip is included with Python 3.4+
+  pip install eventlet
+  pip install flask
+  pip install flask_socketio
+  pip install requests
+  pip install pyOpenSSL==19.1.0
+}

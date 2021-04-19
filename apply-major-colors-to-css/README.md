88a56549845333b8d236ca7e73174f1f5c1f4db6
hchiam <hchiam@users.noreply.github.com>
2021-04-18 22:15:52 -0400

    example: apply major colors to css

---
+++ apply-major-colors-to-css/README.md
@@ -0,0 +1,13 @@
+# to run this example
+
+```powershell
+./_main.ps1 ./image_to_use_as_palette.png
+```
+
+or use a reference image from another folder:
+
+```powershell
+./_main.ps1 ../get-major-colors-in-png/solid_color.png
+```
+
+(See other folders. This code builds on them.)
---
+++ apply-major-colors-to-css/_main.ps1
@@ -0,0 +1,55 @@
+$filename = $args[0]
+$BitMap = [System.Drawing.Bitmap]::FromFile((Resolve-Path $filename).ProviderPath)
+
+$hashTableOfColors = @{}
+foreach($h in 1..$BitMap.Height) {
+  foreach($w in 1..$BitMap.Width) {
+    $hashTableOfColors[$BitMap.GetPixel($w - 1,$h - 1)] += 1
+  }
+}
+
+$palette = $hashTableOfColors.Keys
+# $counts = $hashTableOfColors.Values
+
+$firstCount = 0
+$firstColor = $null
+$secondCount = 0
+$secondColor = $null
+foreach($color in $palette) {
+  $count = $hashTableOfColors[$color]
+  if ($count -gt $firstCount) {
+    $firstCount = $count
+    $firstColor = $color
+  } elseif ($count -gt $secondCount) {
+    $secondCount = $count
+    $secondColor = $color
+  }
+}
+
+# echo -----
+# echo $firstCount
+# echo $firstColor
+# echo -----
+# if ($secondColor) {
+#   echo $secondCount
+#   echo $secondColor
+#   echo -----
+# }
+
+$firstColorName = $firstColor.Name
+$secondColorName = if ($secondColor) { $secondColor.Name } else { '000000' }
+
+echo -----
+echo '2 most dominant colors found:'
+echo "#$firstColorName"
+echo "#$secondColorName"
+echo -----
+
+Remove-Item ./output.css
+
+Copy-Item "input.css" -Destination "output.css"
+
+(Get-Content output.css) `
+    -replace '(--main-color: ).+?;', "`$1#$firstColorName;" `
+    -replace '(--secondary-color: ).+?;', "`$1#$secondColorName;" |
+Out-File output.css
